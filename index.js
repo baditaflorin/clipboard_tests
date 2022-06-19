d19135de96b011b270b164fe844723e1c266a14c
baditaflorin <baditaflorin@gmail.com>
2022-06-20 02:40:27 +0300

    2 files modified

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ import './style.css';
 
 // Write Javascript code!
 function fallbackCopyTextToClipboard(text) {
-  var textArea = document.createElement("textarea");
+  var textArea = document.createElement('textarea');
   textArea.value = text;
-  
+
   // Avoid scrolling to bottom
-  textArea.style.top = "0";
-  textArea.style.left = "0";
-  textArea.style.position = "fixed";
+  textArea.style.top = '0';
+  textArea.style.left = '0';
+  textArea.style.position = 'fixed';
 
   document.body.appendChild(textArea);
   textArea.focus();
@@ -31,36 +31,39 @@ function copyTextToClipboard(text) {
     fallbackCopyTextToClipboard(text);
     return;
   }
-  navigator.clipboard.writeText(text).then(function() {
-    console.log('Async: Copying to clipboard was successful!');
-  }, function(err) {
-    console.error('Async: Could not copy text: ', err);
-  });
+  navigator.clipboard.writeText(text).then(
+    function () {
+      console.log('Async: Copying to clipboard was successful!');
+    },
+    function (err) {
+      console.error('Async: Could not copy text: ', err);
+    }
+  );
 }
-
-var getTextArea=document.querySelector('js-current-clipboard');
-console.log(getTextArea);
 
 var copyBobBtn = document.querySelector('.js-copy-bob-btn'),
   copyJaneBtn = document.querySelector('.js-copy-jane-btn');
 
-copyBobBtn.addEventListener('click', function(event) {
+copyBobBtn.addEventListener('click', function (event) {
   copyTextToClipboard('Bob');
 });
 
-
-copyJaneBtn.addEventListener('click', function(event) {
+copyJaneBtn.addEventListener('click', function (event) {
   copyTextToClipboard('Jane');
 });
 
 function update_textarea() {
-  navigator.clipboard
-  .readText()
-  .then((copiedText) => {
-        console.log(copiedText)
-   });
+  navigator.clipboard.readText().then((copiedText) => {
+    console.log(copiedText);
+    document.getElementById('js-current-clipboard').value = copiedText
+  });
+}
+
+
+function myTimer() {
+  console.log(' each 1 second...');
+  update_textarea();
 
 }
 
-update_textarea();
-
+var myVar = setInterval(myTimer, 1000); //setting the loop with time interval
